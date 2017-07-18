3660425ca88c4c3ebf09cc5b4ab6ef4078760b7b
Anurag Awasthi <trueindiantiger@gmail.com>
2017-07-18 15:55:51 +0530

    Create ZinSql.js

---
+++ ZinSql.js
@@ -0,0 +1,6 @@
+1. Duplicate invoice. Avoid filling form again and again
+2. Copy the whole row and get a text output so you can compare later when you change the invoice a bit
+3. Search column name in any table or all tables
+
+--Advanced--
+1. Associate UI with DB
