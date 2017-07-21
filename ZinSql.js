f371a8a11fe0d2eb0157399f228935c565cbc32e
Anurag Awasthi <trueindiantiger@gmail.com>
2017-07-21 23:50:05 +0530

    Update ZinSql.js

--- ZinSql.js
+++ ZinSql.js
@@ -1,6 +1,10 @@
 1. Duplicate invoice. Avoid filling form again and again
 2. Copy the whole row and get a text output so you can compare later when you change the invoice a bit
 3. Search column name in any table or all tables
+4. Choose columns you wanna see
+  Remove unwanted using x button
+  Add them to a view and save the view
+5. Make nested queries and join easier using graphics
 
 --Advanced--
 1. Associate UI with DB
