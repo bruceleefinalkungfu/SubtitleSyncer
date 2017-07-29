54575c0fb7a039893f0543e48d9e944913a14cc0
Anurag Awasthi <trueindiantiger@gmail.com>
2017-07-29 20:53:00 +0530

    Create ZinTest.java

---
+++ ZinTest.java
@@ -0,0 +1,18 @@
+
+public class ZinTest {
+	ZinFile zinFile = new ZinFile();
+	FileModifier fileModifier = new FileModifier();
+	public static void main(String [] args) {
+		try{
+			ZinTest zinTest = new ZinTest();
+			zinTest.init();
+			System.out.println("done");
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void init() throws Exception{
+		fileModifier.addStringToALine("zin.txt", 2, "import com.ZinUtil;");
+	}
+}
