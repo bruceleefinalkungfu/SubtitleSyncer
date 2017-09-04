97ba8300b6faa99ce7473336c8a2823c639acfee
Anurag Awasthi <trueindiantiger@gmail.com>
2017-09-04 19:11:00 +0530

    Update ZinReflect.java

--- ZinReflect.java
+++ ZinReflect.java
@@ -1,10 +1,47 @@
 package zin.reflect;
 
 import java.beans.Statement;
+import java.io.File;
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import zin.file.ZinSerializer.SerializedObjectBO;
 
 public class ZinReflect {
+	
+	private static final Map<String, Boolean> CONSOLE_PRINTABLE_TYPES;
+	
+	private static final String NEW_LINE = System.getProperty("line.separator");
+	
+	private static final String IDENTATION_STRING;
+	
+	static{
+		// local variables which are destroyed later
+		int numberOfSpace = 4;
+		Class<?>[] printableTypes = {Integer.class, Double.class, Float.class, BigInteger.class, BigDecimal.class, int.class, double.class, float.class,
+							boolean.class, Boolean.class, String.class, StringBuilder.class, StringBuffer.class};
+		
+		CONSOLE_PRINTABLE_TYPES = new HashMap<>();
+		for(Class<?> class1 : printableTypes){
+			CONSOLE_PRINTABLE_TYPES.put(class1.getName(), true);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0 ; i<numberOfSpace ; i++)
+			sb.append(' ');
+		IDENTATION_STRING = sb.toString();
+	}
+	
 	static public boolean isFieldStatic(Field field){
 		if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
 	        return true;
@@ -12,45 +49,64 @@ public class ZinReflect {
 		return false;
 	}
 	
+	/**
+	 * suppose class Employee has private String employeeName..
+	 * @param instanceFromWhichYouWantPrivateField : new Employee()
+	 * @param instanceClass : Employee.class
+	 * @param fieldType : String.class
+	 * @param fieldName : "employeeName"
+	 * @return
+	 * @throws Exception
+	 */
+	public static <T> T getPrivateFieldValue(Object instanceFromWhichYouWantPrivateField, Class<?> instanceClass, Class<T> fieldType, String fieldName) throws Exception{
+		Field f = instanceClass.getDeclaredField(fieldName);
+		f.setAccessible(true);
+		return castObject(fieldType, f.get(instanceFromWhichYouWantPrivateField));
+		//return (T) castObject(fieldType, f.get(castObject(instanceClass, instanceFromWhichYouWantPrivateField);));
+	}
 
 	/**
-	 * write this in toString() ZinReflection.printToString(this.getClass(), this);
-	 * @param class1 : this.getClass()
-	 * @param obj : 'this' instance
+	 * Suppose you wanna check if class Zin implements Serializable
+	 * isClassImplementOrExtend(Serializable.class, Zin.class); 
+	 * @param dadClassOrNot : class you wanna find out if it's superclass of your class or not
+	 * @param yourClass		: class you wanna check
 	 * @return
 	 */
-	static public String printToString(Class class1, Object obj){
-		StringBuilder result = new StringBuilder();
-		String newLine = System.getProperty("line.separator");
-		result.append( class1.getName() );
-		result.append( " = {" );
-		result.append(newLine);
-		//determine fields declared in this class only (no fields of superclass)
-		Field[] fields = class1.getDeclaredFields();
-		//print field names paired with their values
-		for ( Field field : fields  ) {
-			if(isFieldStatic(field))
-				continue;
-			result.append("  ");
-			try {
-				String fieldTypeName = field.getType().getSimpleName();
-				result.append(fieldTypeName+"  ");
-				result.append( field.getName() );
-				result.append(": ");
-				//requires access to private field:
-				result.append(
-						field.get(castObject(class1, obj))
-						);
-			} catch ( IllegalAccessException ex ) {
-				System.out.println(ex);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			result.append(newLine);
-		}
-		result.append("}");
-		return result.toString();
+	public static boolean doesClassImplementOrExtend(Class<?> dadClassOrNot, Class<?> yourClass){
+		return dadClassOrNot.isAssignableFrom(yourClass);
 	}
+	
+	/**
+	 * Suppose Employee class doesn't implement Serializable
+	 * But all its fields Address address, String name, do
+	 * Then it will return a list of
+	 * 		SerializedObjectBO (Address.class, address)
+	 * 		SerializedObjectBO (String.class, name)
+	 * It's not completed yet
+	 * @param type
+	 * @param value
+	 * @return
+	 */
+	public static List<SerializedObjectBO> getVariableListToSerialize(Class<?> type, Object value){
+		List<SerializedObjectBO> outputList = new ArrayList<>();
+		if(doesClassImplementOrExtend(Serializable.class, type)){
+			outputList.add(new SerializedObjectBO(type, value));
+			return outputList;
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * write this in toString() ZinReflection.printToString(this.getClass(), this);
+	 * @param type : this.getClass()
+	 * @param value : 'this' instance
+	 * @return
+	 */
+	static public String printToString(Class<?> type, Object value){
+		return type.getName() + " = "+ ZinPrivate.printToString(type, value, "").toString();
+	}
+	
 	
 	public static <T> T castObject(Class<T> type, Object object) throws Exception{
 		/*
@@ -74,16 +130,70 @@ public class ZinReflect {
 		Statement statement = new Statement(classInstance, methodName, methodParametersArr);
 		statement.execute();
 	}
-	public static void invokeMethod(Class<?> classToBeInstantiated, String methodName, Object[] methodParametersArr) throws Exception{
-		invokeMethod(classToBeInstantiated.newInstance(), methodName, methodParametersArr);
-	}
 	public static void invokeMethod(Object classInstance, Method method, Object[] methodParametersArr) throws Exception{
 		invokeMethod(classInstance, method.getName(), methodParametersArr);
+	}
+	public static void invokeMethod(Class<?> classToBeInstantiated, String methodName, Object[] methodParametersArr) throws Exception{
+		invokeMethod(classToBeInstantiated.newInstance(), methodName, methodParametersArr);
 	}
 	public static void invokeMethod(Class<?> classToBeInstantiated, Method method, Object[] methodParametersArr) throws Exception{
 		invokeMethod(classToBeInstantiated.newInstance(), method.getName(), methodParametersArr);
 	}
+	public static void invokeMethod(String classFullName, String methodName, Object[] methodParametersArr) throws Exception{
+		invokeMethod(getClassInstanceFromFullName(classFullName).newInstance(), methodName, methodParametersArr);
+	}
+	public static Class<?> getClassInstanceFromFullName(String classFullName) throws Exception{
+		// Class.forName() loads and initializes the class.
+		// while classLoader waits loads and delays initialization until class is used
+		URLClassLoader classLoader = null;
+		Class<?> loadedClass = null;
+		try{
+			classLoader = new URLClassLoader(new URL[]{new File("./").toURI().toURL()});
+			loadedClass = classLoader.loadClass(classFullName);
+		} catch(Exception e){
+			throw e;
+		} finally{
+			if(classLoader != null ) classLoader.close();
+		}
+		return loadedClass;
+	}
 	
 	private static class ZinPrivate{
+		public static StringBuilder printToString(Class<?> type, Object value, String identation){
+			StringBuilder result = new StringBuilder();
+			
+			result.append( "{" + NEW_LINE );
+			//determine fields declared in this class only (no fields of superclass)
+			Field[] fields = type.getDeclaredFields();
+			//print field names paired with their values
+			for ( Field field : fields  ) {
+				// SecurityManager can stop us to make a variable accesible
+				field.setAccessible(true);
+				if(isFieldStatic(field))
+					continue;
+				// Identation inside loop
+				try {
+					String fieldTypeName = field.getType().getSimpleName();
+					result.append( IDENTATION_STRING + identation );
+					result.append(fieldTypeName +" "+ field.getName() + ": ");
+					if(isFieldConsolePrintable(field.getType())){
+						result.append( field.get(castObject(type, value)) );
+					} else{
+						// identation is increased
+						result.append(printToString(field.getType(), field.get(castObject(type, value)), identation+IDENTATION_STRING));
+					}
+				} catch ( IllegalAccessException ex ) {
+					System.out.println(ex);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				result.append(NEW_LINE);
+			}
+			result.append(identation+"}");
+			return result;
+		}
+		private static boolean isFieldConsolePrintable(Class<?> fieldType){
+			return CONSOLE_PRINTABLE_TYPES.get(fieldType.getName()) != null;
+		}
 	}
 }
