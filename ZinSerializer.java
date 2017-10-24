b136e474d969d7145aa664efcb56b0ae5c4017c8
Anurag Awasthi <trueindiantiger@gmail.com>
2017-10-24 20:16:28 +0530

    Update ZinSerializer.java

--- ZinSerializer.java
+++ ZinSerializer.java
@@ -5,9 +5,18 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import zin.file.ZinSerializer.SerializedObjectBO;
 import zin.reflect.ZinReflect;
 
+/**
+ * <pre>
+ * When you want to serialize a method call with all their parameters, use this class
+ * use method serializeMethodAndParameters to serialize
+ * use method deserializeMethodAndParametersAndCall to deserialize and call
+ * </pre>
+ * - Change Class to Class<?>
+ * @author anurag.awasthi
+ *
+ */
 public class ZinSerializer {
 
 	public static String DEFAULT_FILE_FORMAT = ".serlz";
@@ -17,10 +26,12 @@ public class ZinSerializer {
 	public static ZinFile zinFile = new ZinFile();
 	
 	/**
-	 * ZinSerializer.serialize(new Object(){}.getClass().getEnclosingMethod(),
+	 * <pre>
+	 * ZinSerializer.serializeMethodAndParameters(new Object(){}.getClass().getEnclosingMethod(),
 	 * 			ZinIO.arrayToList(ServiceRequest.class, ServiceResponse.class),
 	 * 			ZinIO.arrayToList(serviceRequest, serviceResponse));
 	 * Object will be saved into className.methodName.serlz
+	 * </pre>
 	 * @param method : new Object(){}.getClass().getEnclosingMethod()
 	 * @param classes : classes of each method parameters in correct order
 	 * @param objList : All the method parameters in correct order
@@ -31,7 +42,7 @@ public class ZinSerializer {
 	}
 	
 	/**
-	 * ZinSerializer.serialize("zin.serlz", new Object(){}.getClass().getEnclosingMethod(),
+	 * ZinSerializer.serializeMethodAndParameters("zin.serlz", new Object(){}.getClass().getEnclosingMethod(),
 	 * 			ZinIO.arrayToList(ServiceRequest.class, ServiceResponse.class),
 	 * 			ZinIO.arrayToList(serviceRequest, serviceResponse));
 	 * @param fileName
@@ -41,10 +52,7 @@ public class ZinSerializer {
 	 */
 	public static void serializeMethodAndParameters(String fileName, Method method, List<Class> classes, List<Object> objList){
 		try {
-			String shouldSerialize = zinFile.getPropertiesFileValueFromKey("zin.properties", "shouldSerialize");
-			if(shouldSerialize.equals("true")){
-				ZinPrivate.serializeMethodAndParameters(fileName+DEFAULT_FILE_FORMAT, method, classes, objList);
-			}
+				ZinPrivate.serializeMethodAndParameters(fileName, method, classes, objList);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -86,6 +94,56 @@ public class ZinSerializer {
 		}
 	}
 	
+	public static class MethodAndParametersBO implements Serializable{
+
+		private static final long serialVersionUID = 7574699104008578078L;
+		
+		public Class<?> classHavingMethod;
+		public String methodName;
+		public Class<?> parametersTypeArr [];
+		List<SerializedObjectBO> parametersList;
+		public MethodAndParametersBO(Class<?> classHavingMethod, String methodName, Class<?>[] parametersTypeArr,
+				List<SerializedObjectBO> parametersList) {
+			this.classHavingMethod = classHavingMethod;
+			this.methodName = methodName;
+			this.parametersTypeArr = parametersTypeArr;
+			this.parametersList = parametersList;
+		}
+		public MethodAndParametersBO(Method method, List<Object> parameters){
+			this.classHavingMethod = method.getDeclaringClass();
+			this.methodName = method.getName();
+			this.parametersTypeArr = method.getParameterTypes();
+			int len = parametersTypeArr.length;
+			if(parameters.size() != len){
+				throw new IllegalArgumentException("Number of parameters are in method is "+len+" while you sent "+parameters.size()+" params");
+			}
+			List<SerializedObjectBO> serializedObjectBOs = new ArrayList<>();
+			for(int i=0 ; i<len ; i++){
+				serializedObjectBOs.add(new SerializedObjectBO(parametersTypeArr[i], parameters.get(i)));
+			}
+			this.parametersList = serializedObjectBOs;
+		}
+		public Method getMethod() throws NoSuchMethodException, SecurityException{
+			return classHavingMethod.getMethod(methodName, parametersTypeArr);
+		}
+
+		public Object[] getMethodParameterObjectsArr() throws Exception{
+			List<Object> methodParametersList = new ArrayList<>(); 
+			for(SerializedObjectBO objectToSerializedBO : parametersList){
+				methodParametersList.add(objectToSerializedBO.getObject());
+			}
+			return methodParametersList.stream().toArray(Object[]::new);
+		}
+	}
+	
+	/**
+	 * <pre>
+	 * You shouldn't do SerializedObjectBO<T> because then when you prepare a list
+	 * You HAVE TO HAVE of 'Object' List< SerializedObjectBO<Object> > objectToSerializeBOList
+	 * Then you HAVE TO HAVE a cast of your own when calling getObject()
+	 * </pre>
+	 * 
+	 */
 	public static class SerializedObjectBO implements Serializable{
 		
 		private static final long serialVersionUID = 2742188140708251473L;
@@ -103,7 +161,11 @@ public class ZinSerializer {
 
 	
 	private static class ZinPrivate{
+		
 		public static void serializeMethodAndParameters(String fileName, Method method, List<Class> classes, List<Object> objList) throws Exception{
+			MethodAndParametersBO writeBO = new MethodAndParametersBO(method, objList);
+			zinFile.writeObject(fileName, writeBO);
+			/*
 			int size = classes.size();
 			List<SerializedObjectBO> objectToSerializedBOList = new ArrayList<ZinSerializer.SerializedObjectBO>();
 			for(int i=0 ; i<size ; i++){
@@ -115,28 +177,22 @@ public class ZinSerializer {
 			objectListToWrite.add(method.getParameterTypes());
 			objectListToWrite.add(objectToSerializedBOList);
 			zinFile.writeObject(fileName, objectListToWrite);
+			//*/
 		}
-		@SuppressWarnings("unchecked")
+		
 		public static void deserializeMethodAndParametersAndCall(String fileName) throws Exception{
+			MethodAndParametersBO readBO = (MethodAndParametersBO) zinFile.readObject(fileName);
+			Method method = readBO.getMethod();
+			Object[] methodParametersArr = readBO.getMethodParameterObjectsArr();
+			ZinReflect.invokeMethod(readBO.classHavingMethod, method, methodParametersArr);
+			/*
 			List<Object> objList = (List<Object>) zinFile.readObject(fileName);
 			Method method = deserializeMethod(objList);
-			List<SerializedObjectBO> objectToSerializeBOList = (List<SerializedObjectBO>) objList.get(3);
+			List<SerializedObjectBO> objectToSerializeBOList = (List<>) objList.get(3);
 			Object[] methodParametersArr = getMethodParametersArray(objectToSerializeBOList);
 			Class<?> classToBeInstantiated = (Class<?>)objList.get(0);
 			ZinReflect.invokeMethod(classToBeInstantiated, method, methodParametersArr);
-		}
-		private static Method deserializeMethod(List<Object> objList) throws Exception{
-			Class<?> declaringClass = (Class<?>) objList.get(0);
-			String methodName = (String) objList.get(1);
-			Class<?>[] parameterTypes = (Class<?>[]) objList.get(2);
-			return declaringClass.getMethod(methodName, parameterTypes);
-		}
-		private static Object[] getMethodParametersArray(List<SerializedObjectBO> objectToSerializeBOList) throws Exception{
-			List<Object> methodParametersList = new ArrayList<>(); 
-			for(SerializedObjectBO objectToSerializedBO : objectToSerializeBOList){
-				methodParametersList.add(objectToSerializedBO.getObject());
-			}
-			return methodParametersList.stream().toArray(Object[]::new);
+			//*/
 		}
 	}
 }
